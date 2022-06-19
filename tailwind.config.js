ea765057393beb5fa1442def13aba6cbc4c41196
isaaan98 <ikhsanmhd0610@gmail.com>
2022-06-19 15:31:46 +0700

    Portfolio with tailwind css v3

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    // screens: {
+    //   'sm': '576px',
+    //   // => @media (min-width: 576px) { ... }
+
+    //   'md': '960px',
+    //   // => @media (min-width: 960px) { ... }
+
+    //   'lg': '1440px',
+    //   // => @media (min-width: 1440px) { ... }
+    // },
+    container: {
+      center: true,
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
